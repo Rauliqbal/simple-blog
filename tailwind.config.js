787261b6742c526a8407230b4e6b40058ad061cd
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2023-09-30 22:24:24 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins : ["Poppins",'sans-serif'],
+        jetBrains : ["JetBrains Mono", "monospace"]
+      },
+      container : {
+        center: true,
+        padding: "16px",
+        screens: {
+          md: "768px",
+          lg: "1024px",
+          xl: "1060px",
+          
+       },
+      }
+    },
+  },
+  plugins: [require('@tailwindcss/typography'),
+],
+}
+
